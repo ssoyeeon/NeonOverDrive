d85b49f38f6179ce2aed33d79c3d90a79837a440
ssoyeeon <ssuu2039@gmail.com>
2025-04-20 02:41:19 +0900

    [feat] GameManager

--- NeonOverDrive/Assets/Scripts/Manager/GameManager.cs
+++ NeonOverDrive/Assets/Scripts/Manager/GameManager.cs
@@ -7,17 +7,40 @@ public class GameManager : MonoBehaviour
     private static GameManager _instance;
     public static GameManager Instance => _instance;
 
+    public GameState CurrentGameState { get; private set; }
+
+    public PlayerData PlayerData { get; private set; }
     
     public int money;       //ÃÑ ³» µ·
     public bool isEnd;      //³¡³µ´ÂÁö
     public int reward;      //º¸»ó
 
+    private void Awake()
+    {
+        if(_instance == null)
+            _instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+
+        //LoadPlayerData();
+    }
+    public void ChangeGameState(GameState newState)
+    {
+        CurrentGameState = newState;
+        
+    }
+    public void GoToGarage()
+    {
+        ChangeGameState(GameState.Garage);
+    }
+    public void GoToStageSelect()
+    {
+        ChangeGameState(GameState.StageSelect);
+    }
     void Start()
     {
         isEnd = false;
-        if (gameObject == null) Debug.LogError("Null GameManager");
-        else Destroy(gameObject);
-        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
